00d07801b9e0eacfd845db34a05f33b9c3f1d89f
Conquer5 <165409213+Conquer5@users.noreply.github.com>
2025-01-09 02:04:50 +0700

    Update rf.py

--- rf.py
+++ rf.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import joblib
-
+from imblearn.over_sampling import SMOTE
 
 import numpy as np
 import pandas as pd
@@ -177,6 +177,10 @@ for col in df.columns:
 # Split data
 X = df.drop(columns=[target_col])
 y = df[target_col]
+
+smote = SMOTE(random_state=42)
+X, y = smote.fit_resample(X, y)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train model
